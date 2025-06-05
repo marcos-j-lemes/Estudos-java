149894d0e7074fa1d7e27fe6d1a5d4a71d9f3ed7
marcos-j-lemes <marcos.jlf@aluno.ifsc.edu.br>
2025-06-05 19:39:46 -0300

    config-string

--- Aula-02 - String/Stringg.java
+++ Aula-02 - String/Stringg.java
@@ -1,15 +1,14 @@
-public class String {
-    public class Scanner{
+public class Stringg{
 
-    private static char letra = 'A';
-    public static char letraB = 'B';
-    
+   private static char letra = 'A';
+   public static char letraB = 'B';
+
     public static void main(String[] args){
+
+        //String nome = "Marcos";
 
         System.out.println("Print letter: "+ letra);
         System.out.println("Print char: "+ letraB);
 
     } 
-}
-    
 }
