e4ea9b8e954e46b83a6c2dbb00b3afb15c94fef2
marcos-j-lemes <marcos.jlf@aluno.ifsc.edu.br>
2025-06-05 19:30:11 -0300

    String

---
+++ Aula-02 - String/Notas.md
@@ -0,0 +1,4 @@
+# String
+
+
+É uma sequencia de caracter que forma uma string, sendo uma classe no java onde os dados são tratados como uma string
---
+++ Aula-02 - String/String.java
@@ -0,0 +1,15 @@
+public class String {
+    public class Scanner{
+
+    private static char letra = 'A';
+    public static char letraB = 'B';
+    
+    public static void main(String[] args){
+
+        System.out.println("Print letter: "+ letra);
+        System.out.println("Print char: "+ letraB);
+
+    } 
+}
+    
+}
